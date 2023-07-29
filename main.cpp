cafea147ab5f2e1080b24b2edfca7ae12bdd40b8
Wendell <rapha3lsouza@gmail.com>
2023-07-29 15:47:19 -0400

    importei um video

--- main.cpp
+++ main.cpp
@@ -6,11 +6,28 @@
 using namespace std;
 using namespace cv;
 
+//void main() {
+
+//	string path = "imagens/monalisa.jpg";
+//	Mat img = imread(path);
+//	imshow("Monalisa", img);
+//	waitKey(0);
+//
+//}
+
+
 void main() {
 
-	string path = "imagens/monalisa.jpg";
-	Mat img = imread(path);
-	imshow("Monalisa", img);
-	waitKey(0);
+	string path = "videos/testedevideo.mp4";
+	VideoCapture cap(path);
+	Mat video;
 
+	while(true){
+	
+		cap.read(video);
+
+		imshow("video", video);
+		waitKey(20);
+
+	}
 }
